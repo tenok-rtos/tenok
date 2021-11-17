485574e42fb67c1a0ec122a161e8df9fb4ac91e6
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-11-17 20:26:32 +0800

    BSP: Add led driver

--- src/main.c
+++ src/main.c
@@ -37,8 +37,24 @@ void task2(void *param)
 	}
 }
 
+void led_init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {
+		.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType =GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_DOWN
+	};
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+}
+
 int main(void)
 {
+	led_init();
 	uart3_init();
 
 	task_register(task1, "task1", 0, NULL, &tcb1);
