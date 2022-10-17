f4e65d0760a47930c9ed91b7a8ab58d23000fcef
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-10-17 20:54:58 +0800

    rewrite the scheduler and use round robin algorithm for now

--- src/kernel.c
+++ src/kernel.c
@@ -23,12 +23,11 @@ void sys_getpid(void);
 void sys_mkdir(void);
 void sys_rmdir(void);
 
-tcb_t *tasks[OS_MAX_PRIORITY] = {NULL};
-int task_nums[OS_MAX_PRIORITY] = {0};
+tcb_t tasks[TASK_NUM_MAX];
+int task_nums = 0;
 
-tcb_t *curr_tcb = NULL;
-
-tcb_t tcb_idle_task;
+tcb_t *curr_task = NULL;
+int curr_task_index = 0;
 
 syscall_info_t syscall_table[] = {
 	DEF_SYSCALL(fork, 1),
@@ -46,134 +45,69 @@ syscall_info_t syscall_table[] = {
 
 void task_idle(void *param)
 {
-	/* idle task is a task with the lowest priority.
-	 * when the os has nothing to do, the idle task
-	 * will be executed. */
 	while(1);
 }
 
-void task_register(task_function_t task_func,
-                   const char *task_name,
-                   uint32_t stack_depth,
-                   void * const task_params,
-                   uint32_t priority,
-                   tcb_t *new_task)
+void task_create(task_function_t task_func, uint32_t priority)
 {
-	/* check if the task number exceeded the limit */
-	uint32_t total_task_num = 0;
-
-	int i;
-	for(i = 0; i < OS_MAX_PRIORITY; i++) {
-		total_task_num += task_nums[i];
-	}
-
-	if(total_task_num >= TASK_NUM_MAX) {
+	if(task_nums > TASK_NUM_MAX) {
 		return;
 	}
 
-	/* append new task to the end of the list */
-	if(tasks[priority] == NULL) {
-		/* if the task list is empty */
-		tasks[priority] = new_task;
-		tasks[priority]->next = new_task;
-		tasks[priority]->last = new_task;
-	} else {
-		/* append new task at the end of the list */
-		new_task->next = tasks[priority];
-		new_task->last = tasks[priority]->last;
-		tasks[priority]->last->next = new_task;
-		tasks[priority]->last = new_task;
-	}
-	task_nums[priority]++; //increase the task number
+	tasks[task_nums].status = TASK_READY;
+	tasks[task_nums].priority = priority;
 
-	stack_depth = TASK_STACK_SIZE; //TODO: variable size?
-	new_task->status = TASK_READY;
-	new_task->priority = priority;
-
-	/* initialize task name */
-	for(i = 0; i < TASK_NAME_LEN_MAX; i++) {
-		new_task->task_name[i] = task_name[i];
-
-		if(task_name[i] == '\0') {
-			break;
-		}
-	}
-	new_task->task_name[TASK_NAME_LEN_MAX - 1] = '\0';
-
-	/*===========================*
-	 * initialize the task stack *
-	 *===========================*/
+	uint32_t stack_depth = TASK_STACK_SIZE; //TODO: variable size?
 
 	/* stack design contains three parts:
 	 * xpsr, pc, lr, r12, r3, r2, r1, r0, (for setting exception return),
-         * _r7 (for passing system call number), and
+	 * _r7 (for passing system call number), and
 	 * _lr, r11, r10, r9, r8, r7, r6, r5, r4 (for context switch) */
-
-	uint32_t *stack_top = new_task->stack + stack_depth;
-	stack_top -= 18;
+	uint32_t *stack_top = tasks[task_nums].stack + stack_depth - 18;
 	stack_top[17] = INITIAL_XPSR;
 	stack_top[16] = (uint32_t)task_func; // lr = task_entry
 	stack_top[8]  = THREAD_PSP;          //_lr = 0xfffffffd
+	tasks[task_nums].stack_top = (user_stack_t *)stack_top;
 
-	new_task->stack_top = (user_stack_t *)stack_top;
+	task_nums++;
 }
 
 void schedule(void)
 {
-	int i, j;
-	tcb_t *_task;
+	int i;
 
-	bool change_task = false;   //flag that indicates the scheduler should select next task
-	bool task_selected = false; //next task to be executed is decieded
+	/* update sleep timers */
+	for(i = 0; i < task_nums; i++) {
+		if(tasks[i].ticks_to_delay > 0) {
+			tasks[i].ticks_to_delay--;
 
-	/* if current task is the idle task or it is no longer in running state then it should
-	 * yield the cpu to other tasks */
-	if(curr_tcb->status != TASK_RUNNING || curr_tcb == &tcb_idle_task) {
-		change_task = true;
-	}
-
-	/* find the next ready task with the highest priority */
-	for(i = OS_MAX_PRIORITY - 1; i >= 0; i--) {
-		/* current task with prioity number i is empty */
-		if(tasks[i] == NULL) {
-			continue; //skip
-		}
-
-		/* initialize the task iterator */
-		_task = tasks[i];
-
-		/* iterate through the task list */
-		for(j = 0; j < task_nums[i]; j++) {
-			if((_task->priority > curr_tcb->priority || change_task == true) &&
-			    _task != curr_tcb &&
-			    _task->status == TASK_READY &&
-			    task_selected == false) {
-				//change status of the current task
-				if(curr_tcb->status == TASK_RUNNING) {
-					curr_tcb->status = TASK_READY;
-				}
-
-				//next task to run is selected
-				task_selected = true;
-				curr_tcb = _task;
-				curr_tcb->status = TASK_RUNNING;
+			/* task is ready */
+			if(tasks[i].ticks_to_delay == 0) {
+				tasks[i].status = TASK_READY;
 			}
-
-			/* update the remained delay ticks */
-			if(_task->ticks_to_delay > 0) {
-				_task->ticks_to_delay--;
-
-				/* delay is finished, change the task state to
-				 * be ready */
-				if(_task->ticks_to_delay == 0) {
-					_task->status = TASK_READY;
-				}
-			}
-
-			/* update the task iterator */
-			_task = _task->next;
 		}
 	}
+
+	/* change status of the current task */
+	if(curr_task->status == TASK_RUNNING) {
+		curr_task->status = TASK_READY;
+	}
+
+	/* round robin */
+	while(1) {
+		curr_task_index++;
+		if(curr_task_index >= task_nums) {
+			curr_task_index = 0;
+		}
+
+		if(tasks[curr_task_index].status == TASK_READY) {
+			break;
+		}
+	}
+
+	/* assign the next task to be executed */
+	curr_task = &tasks[curr_task_index];
+	curr_task->status = TASK_RUNNING;
 }
 
 void sys_fork(void)
@@ -182,11 +116,11 @@ void sys_fork(void)
 
 void sys_sleep(void)
 {
-	curr_tcb->ticks_to_delay = curr_tcb->stack_top->r0;
-	curr_tcb->status = TASK_WAIT;
+	curr_task->ticks_to_delay = curr_task->stack_top->r0;
+	curr_task->status = TASK_WAIT;
 
-	curr_tcb->stack_top->_r7 = 0;
-	curr_tcb->stack_top->r0 = 0; //retval of the syscall
+	curr_task->stack_top->_r7 = 0;
+	curr_task->stack_top->r0 = 0; //retval of the syscall
 }
 
 void sys_open(void)
@@ -230,35 +164,26 @@ void os_start(void)
 	uint32_t stack_empty[32]; //a dummy stack for os enviromnent initialization
 	os_env_init((uint32_t)(stack_empty + 32) /* point to the top */);
 
-	/* register a idle task that do nothing */
-	task_register(task_idle, "idle task", 0, NULL, 0, &tcb_idle_task);
-
-	/* find a task with the highest priority to start */
-	int i;
-	for(i = OS_MAX_PRIORITY - 1; i >= 0; i--) {
-		if(tasks[i] != NULL) {
-			curr_tcb = tasks[i];
-			curr_tcb->status = TASK_RUNNING;
-			break;
-		}
-	}
+	/* create a idle task that do nothing */
+	task_create(task_idle, 0);
 
 	/* initialize systick timer */
 	SysTick_Config(SystemCoreClock / OS_TICK_FREQUENCY);
 
 	int syscall_table_size = sizeof(syscall_table) / sizeof(syscall_info_t);
 
+	int i;
 	while(1) {
+		schedule();
+
+		curr_task->stack_top = (user_stack_t *)jump_to_user_space((uint32_t)curr_task->stack_top);
+
 		/* system call handler */
 		for(i = 0; i < syscall_table_size; i++) {
-			if(curr_tcb->stack_top->_r7 == syscall_table[i].num) {
+			if(curr_task->stack_top->_r7 == syscall_table[i].num) {
 				syscall_table[i].syscall_handler();
 				break;
 			}
 		}
-
-		schedule();
-
-		curr_tcb->stack_top = (user_stack_t *)jump_to_user_space((uint32_t)curr_tcb->stack_top);
 	}
 }
--- src/os_config.h
+++ src/os_config.h
@@ -1,14 +1,10 @@
-#ifndef __CONFIG_RTOS_H__
-#define __CONFIG_RTOS_H__
+#ifndef __OS_CONFIG_H__
+#define __OS_CONFIG_H__
 
-#define OS_TICK_FREQUENCY        (1000)
+#define OS_TICK_FREQUENCY (1000) //Hz
 
-#define TASK_NUM_MAX             (10)
-
-#define TASK_NAME_LEN_MAX        (100) //bytes
-#define TASK_STACK_SIZE          (512) //words
-
-#define OS_MAX_PRIORITY          (16)
-#define SYSCALL_INT_PRIORITY_MAX (5)
+#define TASK_STACK_SIZE   (512)  //words
+#define TASK_NUM_MAX      (10)
+#define TASK_MAX_PRIORITY (5)
 
 #endif
