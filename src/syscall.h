064fb4001e0594f394c21ccd0d24f995b922a5c5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-10-17 19:31:02 +0800

    Implement system call handler

--- src/syscall.h
+++ src/syscall.h
@@ -1,6 +1,14 @@
 #ifndef __SYSCALL_H__
 #define __SYSCALL_H__
 
+#define DEF_SYSCALL(func, _num) \
+	{.syscall_handler = sys_ ## func, .num = _num}
+
+typedef struct {
+	void (* syscall_handler)(void);
+	uint32_t num;
+} syscall_info_t;
+
 int fork();
 uint32_t sleep(uint32_t ticks);
 
