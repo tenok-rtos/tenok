b8ee901c9c804c83ca141e21e4400bb912c14c9a
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-10-24 14:49:50 +0800

    implement container_of() and linked list pop

--- src/list.h
+++ src/list.h
@@ -1,9 +1,19 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
+#include <stddef.h>
+
+#define container_of(ptr, type, member) \
+	((type *)((void *)ptr - offsetof(type, member)))
+
 typedef struct list {
 	struct list *last;
 	struct list *next;
 } list_t;
+
+void list_init(list_t *list);
+int list_is_empty(list_t *list);
+void list_remove(list_t *list);
+void list_push(list_t *list, list_t *new);
 
 #endif
