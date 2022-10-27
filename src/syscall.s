11374e5804d15879cd1e700a1e8cf4b10b8bdc31
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-10-27 16:44:47 +0800

    add syscall prototype for semiphore functions

--- src/syscall.s
+++ src/syscall.s
@@ -15,6 +15,21 @@
 .type getpid, %function
 .global getpid
 
+.type mkdir, %function
+.global mkdir
+
+.type rmdir, %function
+.global rmdir
+
+.type sem_init, %function
+.global sem_init
+
+.type sem_post, %function
+.global sem_post
+
+.type sem_wait, %function
+.global sem_wait
+
 .macro syscall syscall_num
 	push {r7}            //preserve old r7 for overwriting
 	mov r7, \syscall_num //write syscall number to r7 as new value
@@ -38,3 +53,18 @@ setpriority:
 
 getpid:
 	syscall #9
+
+mkdir:
+	syscall #10
+
+rmdir:
+	syscall #11
+
+sem_init:
+	syscall #12
+
+sem_post:
+	syscall #13
+
+sem_wait:
+	syscall #14
