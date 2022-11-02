f5bb2c694efefd2840631e729bde84534dad7c84
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-02 21:57:35 +0800

    Adjust declaration of file operations for FIFO

--- src/fifo.c
+++ src/fifo.c
@@ -7,10 +7,8 @@ ssize_t fifo_read(struct file *filp, char *buf, size_t size, long offset);
 ssize_t fifo_write(struct file *filp, const char *buf, size_t size, long offset);
 
 static struct file_operations fifo_ops = {
-read:
-	fifo_read,
-write:
-	fifo_write
+	.read = fifo_read,
+	.write = fifo_write
 };
 
 ssize_t fifo_read(struct file *filp, char *buf, size_t size, long offset)
