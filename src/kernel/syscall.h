7be66ce8e461dc3348b14a1b7730dc51b1ac383e
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-03 18:15:53 +0800

    implement read and write syscall

--- src/kernel/syscall.h
+++ src/kernel/syscall.h
@@ -17,8 +17,11 @@ typedef struct {
 uint32_t sem_up(uint32_t *_lock, uint32_t new_val);
 uint32_t sem_down(uint32_t *_lock, uint32_t new_val);
 
+/* syscall function prototypes */
 int fork(void);
 uint32_t sleep(uint32_t ticks);
+ssize_t read(int fd, void *buf, size_t count);
+ssize_t write(int fd, const void *buf, size_t count);
 uint32_t getpriority(void);
 int setpriority(int which, int who, int prio);
 int getpid(void);
