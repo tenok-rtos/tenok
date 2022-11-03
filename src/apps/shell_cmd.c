c64b3bb7a80e8579fa629160c7818b5294c38c6c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-03 21:05:36 +0800

    create shell_cmd.c

---
+++ src/apps/shell_cmd.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+
+void shell_cmd_help(char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX], int param_cnt)
+{
+	char *s = "supported commands:\n\r"
+	          "help\n\r"
+	          "clear\n\r";
+	shell_puts(s);
+}
+
+void shell_cmd_clear(char param_list[PARAM_LIST_SIZE_MAX][PARAM_LEN_MAX], int param_cnt)
+{
+	shell_cls();
+}
