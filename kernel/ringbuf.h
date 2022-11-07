639b23edae74e95ec86b97780d787bfc148319a4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-07 20:09:09 +0800

    Add missing commit for ring buffer

--- kernel/ringbuf.h
+++ kernel/ringbuf.h
@@ -10,16 +10,16 @@ struct ringbuf {
 	int     start;
 	int     end;
 	int     count;
-	uint8_t *data;
+	void    *data;
 	size_t  ring_size;
 	size_t  type_size;
 
 	struct file file;
 };
 
-void ringbuf_init(struct ringbuf *rb, uint8_t *data, size_t type_size, size_t ring_size);
-void ringbuf_put(struct ringbuf *rb, const uint8_t *data);
-void ringbuf_get(struct ringbuf *rb, uint8_t *data);
+void ringbuf_init(struct ringbuf *rb, void *data, size_t type_size, size_t ring_size);
+void ringbuf_put(struct ringbuf *rb, const void *data);
+void ringbuf_get(struct ringbuf *rb, void *data);
 bool ringbuf_is_empty(struct ringbuf *rb);
 bool ringbuf_is_full(struct ringbuf *rb);
 
