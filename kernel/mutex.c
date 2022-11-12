a25960369a21836b153e31162193090e6e86e5c5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-13 00:22:25 +0800

    Implement POSIX mutex

---
+++ kernel/mutex.c
@@ -0,0 +1,61 @@
+#include <stddef.h>
+#include "kernel.h"
+#include "list.h"
+#include "mutex.h"
+#include "syscall.h"
+
+extern tcb_t *running_task;
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutex_attr_t *attr)
+{
+	mutex->owner = NULL;
+	list_init(&mutex->wait_list);
+
+	return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+	spin_lock_irq(&mutex->lock);
+
+	int retval = 0;
+
+	/* check mutex owner */
+	if(mutex->owner == running_task) {
+		/* release the mutex */
+		mutex->owner = NULL;
+
+		/* check the mutex waiting list */
+		if(!list_is_empty(&mutex->wait_list)) {
+			/* wake up a task from the mutex waiting list */
+			wake_up(&mutex->wait_list);
+		}
+	} else {
+		retval = EPERM; 
+	}
+
+	spin_unlock_irq(&mutex->lock);
+
+	return retval;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+	spin_lock_irq(&mutex->lock);
+
+	if(mutex->owner != NULL) {
+		/* put the current task into the mutex waiting list */
+		prepare_to_wait(&mutex->wait_list, &running_task->list, TASK_WAIT);
+
+		spin_unlock_irq(&mutex->lock);
+
+		yield(); //yield the cpu and back to the kernel
+	} else {
+		/* set new owner of the mutex */
+		mutex->owner = running_task;
+
+		spin_unlock_irq(&mutex->lock);
+	}
+
+	return 0;
+} 
