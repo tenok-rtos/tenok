1c5995d0f5fef8e9982d833d32a00291b7de60ff
Shengwen Cheng <shengwen1997.tw@gmail.com>
2022-11-13 03:53:22 +0800

    Link message queue with syscall handler

--- kernel/mqueue.h
+++ kernel/mqueue.h
@@ -7,6 +7,13 @@
 
 typedef int mqd_t;
 
-int mq_init(int fd, struct file **files, struct memory_pool *mem_pool);
+struct mq_attr {
+	long mq_flags;   //flags: 0 or O_NONBLOCK
+	long mq_maxmsg;  //max number of the messages in the queue
+	long mq_msgsize; //byte size of the message
+	long mq_curmsgs; //number of the messages currently in the queue
+};
+
+int mq_init(int fd, struct file **files, struct mq_attr *attr, struct memory_pool *mem_pool);
 
 #endif
