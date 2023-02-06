07db491472f01b0a4fa9935c3d224899e78d78bb
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-07 04:10:29 +0800

    enable fpu by default

--- lib/CMSIS/system_stm32f4xx.c
+++ lib/CMSIS/system_stm32f4xx.c
@@ -238,9 +238,9 @@ void SystemInit(void)
 #endif
 
 #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
-  SCB->CPACR |= ((3UL << 10*2) | /* set CP10 Full Access */
-                 (3UL << 11*2)); /* set CP11 Full Access */
-  #endif
+//  SCB->CPACR |= ((3UL << 10*2) | /* set CP10 Full Access */
+//                 (3UL << 11*2)); /* set CP11 Full Access */
+#endif
 }
 
 /**
