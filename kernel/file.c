676eb72ed970616553147df5a05f5d9e34ac28ea
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-08 00:38:58 +0800

    implement the path server

--- kernel/file.c
+++ kernel/file.c
@@ -30,31 +30,44 @@ void path_server(void)
 		read(PATH_SERVER_FD, &reply_fd, sizeof(reply_fd));
 
 		switch(file_cmd) {
-		case PATH_CMD_MKFILE:
+		case PATH_CMD_REGISTER_PATH:
 			read(PATH_SERVER_FD, &path_len, sizeof(path_len));
 			read(PATH_SERVER_FD, path, path_len);
-			read(PATH_SERVER_FD, &dev, sizeof(dev));
 
-			/* register new node */
-			if(mknod(path, 0, dev) == 0) {
+			int new_fd;
+
+			if(path_cnt < FILE_CNT_LIMIT) {
 				memcpy(path_list[path_cnt], path, path_len);
+
+				new_fd = path_cnt + 1;
 				path_cnt++;
+			} else {
+				new_fd = -1;
 			}
+
+			write(reply_fd, &new_fd, sizeof(new_fd));
+
 			break;
 		case PATH_CMD_OPEN:
 			read(PATH_SERVER_FD, &path_len, sizeof(path_len));
 			read(PATH_SERVER_FD, path, path_len);
 
+			int open_fd;
+
 			/* invalid empty path */
 			if(path[0] == '\0') {
+				open_fd = -1;
 			}
 
 			/* path finding */
 			int i;
 			for(i = 0; i < path_cnt; i++) {
 				if(strcmp(path_list[i], path) == 0) {
+					open_fd = i + 1;
 				}
 			}
+
+			write(reply_fd, &open_fd, sizeof(open_fd));
 
 			break;
 		}
