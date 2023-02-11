dc4f648ea3afaba4674e636c6291ae85f0763141
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-11 21:20:06 +0800

    add file operations for the rom device

--- kernel/rom_dev.c
+++ kernel/rom_dev.c
@@ -1,7 +1,32 @@
+#include <stddef.h>
+#include "file.h"
+
 extern char _section_rom_start;
 extern char _section_rom_end;
+
+long rom_dev_llseek(struct file *filp, long offset, int whence);
+ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset);
+ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset);
+
+static struct file_operations rom_dev_ops = {
+	.llseek = rom_dev_llseek,
+	.read = rom_dev_read,
+	.write = rom_dev_write
+};
 
 int rom_dev_init(void)
 {
 	char *start_addr = &_section_rom_start;
+}
+
+ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset)
+{
+}
+
+ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset)
+{
+}
+
+long rom_dev_llseek(struct file *filp, long offset, int whence)
+{
 }
