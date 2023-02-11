0471ceb512514262e7bc74c32195fa8d08c6fe29
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-11 19:38:58 +0800

    create rom device

---
+++ kernel/rom_dev.c
@@ -0,0 +1,7 @@
+extern char _section_rom_start;
+extern char _section_rom_end;
+
+int rom_dev_init(void)
+{
+	char *start_addr = &_section_rom_start;
+}
---
+++ kernel/rom_dev.h
@@ -0,0 +1,6 @@
+#ifndef __ROM_DEV_H__
+#define __ROM_DEV_H__
+
+int rom_dev_init(void);
+
+#endif
