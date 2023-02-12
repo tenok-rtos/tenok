d466c60250de41c99834680e5edb151186bd962f
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-12 15:16:29 +0800

    Implement rom_dev file operations

--- kernel/rom_dev.c
+++ kernel/rom_dev.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include <stddef.h>
+#include <string.h>
+#include <errno.h>
 #include "file.h"
 
 extern char _section_rom_start;
@@ -8,6 +11,8 @@ long rom_dev_llseek(struct file *filp, long offset, int whence);
 ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset);
 ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset);
 
+char *rom_data_ptr = NULL;
+
 static struct file_operations rom_dev_ops = {
 	.llseek = rom_dev_llseek,
 	.read = rom_dev_read,
@@ -16,17 +21,50 @@ static struct file_operations rom_dev_ops = {
 
 int rom_dev_init(void)
 {
-	char *start_addr = &_section_rom_start;
+	char *rom_data_ptr = &_section_rom_start;
 }
 
 ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset)
 {
+	if((rom_data_ptr + size) > &_section_rom_end) {
+		return EFAULT;
+	}
+
+	memcpy(buf, rom_data_ptr, sizeof(char) * size);
+	rom_data_ptr += size;
+
+	return size;
 }
 
 ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset)
 {
+	return 0;
 }
 
 long rom_dev_llseek(struct file *filp, long offset, int whence)
 {
+	char *new_pos;
+
+	switch(whence) {
+	case SEEK_SET:
+		new_pos = &_section_rom_start + offset;
+		break;
+	case SEEK_END:
+		new_pos = &_section_rom_end + offset;
+		break;
+	case SEEK_CUR:
+		new_pos = rom_data_ptr + offset;
+		break;
+	default:
+		return -1;
+	}
+
+	if((new_pos >= &_section_rom_start) && (new_pos <= &_section_rom_end)) {
+		rom_data_ptr = new_pos;
+
+		long offset_from_start = new_pos - &_section_rom_start;
+		return offset_from_start;
+	} else {
+		return -1;
+	}
 }
