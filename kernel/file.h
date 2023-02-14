4f8c13642b7bd596b9a31749d4baa1eca43f73d7
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-15 07:58:31 +0800

    save file type into the inode

--- kernel/file.h
+++ kernel/file.h
@@ -53,8 +53,8 @@ void file_system_init(void);
 int register_chrdev(char *name, struct file_operations *fops);
 int register_blkdev(char *name, struct file_operations *fops);
 
-void request_path_register(int reply_fd, char *path);
-void request_file_open(int reply_fd, char *path);
+void request_create_file(int reply_fd, char *path, uint8_t file_type);
+void request_open_file(int reply_fd, char *path);
 
 void file_system(void);
 
