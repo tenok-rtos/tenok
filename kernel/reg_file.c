d24a54409938c94d1f40a95b007e7705ccc5f481
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-15 09:02:59 +0800

    define reg_file structure

--- kernel/reg_file.c
+++ kernel/reg_file.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <errno.h>
 #include "file.h"
+#include "mpool.h"
 #include "syscall.h"
+#include "reg_file.h"
 
 long reg_file_llseek(struct file *filp, long offset, int whence);
 ssize_t reg_file_read(struct file *filp, char *buf, size_t size, loff_t offset);
@@ -14,6 +16,16 @@ static struct file_operations reg_file_ops = {
 	.read = reg_file_read,
 	.write = reg_file_write
 };
+
+int reg_file_init(int fd, struct file **files, struct file *driver_file, struct memory_pool *mem_pool)
+{
+	struct reg_file *reg_file = memory_pool_alloc(mem_pool, sizeof(struct reg_file));
+	reg_file->file.f_op = &reg_file_ops;
+	reg_file->driver_file = driver_file; //file driver: e.g., rootfs, romfs, etc.
+	files[fd] = &reg_file->file;
+
+	return 0;
+}
 
 ssize_t reg_file_read(struct file *filp, char *buf, size_t size, loff_t offset)
 {
