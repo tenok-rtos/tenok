3d82010381bd8fd8a480a545d4f3589ec7c2a56b
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-17 19:36:26 +0800

    Support ramdom access for ROM device

--- kernel/rom_dev.c
+++ kernel/rom_dev.c
@@ -9,14 +9,10 @@
 extern char _section_rom_start;
 extern char _section_rom_end;
 
-long rom_dev_llseek(struct file *filp, long offset, int whence);
 ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset);
 ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset);
 
-char *rom_data_ptr = NULL;
-
 static struct file_operations rom_dev_ops = {
-	.llseek = rom_dev_llseek,
 	.read = rom_dev_read,
 	.write = rom_dev_write
 };
@@ -24,18 +20,17 @@ static struct file_operations rom_dev_ops = {
 int rom_dev_init(void)
 {
 	register_blkdev("rom", &rom_dev_ops);
-
-	rom_data_ptr = &_section_rom_start;
 }
 
 ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset)
 {
-	if((rom_data_ptr + size) > &_section_rom_end) {
+	char *read_addr = &_section_rom_start + offset;
+
+	if((read_addr + size) > &_section_rom_end) {
 		return EFAULT;
 	}
 
-	memcpy(buf, rom_data_ptr, sizeof(char) * size);
-	rom_data_ptr += size;
+	memcpy(buf, read_addr, sizeof(char) * size);
 
 	return size;
 }
@@ -43,32 +38,4 @@ ssize_t rom_dev_read(struct file *filp, char *buf, size_t size, loff_t offset)
 ssize_t rom_dev_write(struct file *filp, const char *buf, size_t size, loff_t offset)
 {
 	return 0;
-}
-
-long rom_dev_llseek(struct file *filp, long offset, int whence)
-{
-	char *new_pos;
-
-	switch(whence) {
-	case SEEK_SET:
-		new_pos = &_section_rom_start + offset;
-		break;
-	case SEEK_END:
-		new_pos = &_section_rom_end + offset;
-		break;
-	case SEEK_CUR:
-		new_pos = rom_data_ptr + offset;
-		break;
-	default:
-		return -1;
-	}
-
-	if((new_pos >= &_section_rom_start) && (new_pos <= &_section_rom_end)) {
-		rom_data_ptr = new_pos;
-
-		long offset_from_start = new_pos - &_section_rom_start;
-		return offset_from_start;
-	} else {
-		return -1;
-	}
 }
