c731b904f73b59cb20a66c870c0956889396ad10
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-18 00:41:05 +0800

    move `container_of' macro into the list.h

--- common/util.h
+++ common/util.h
@@ -1,9 +1,6 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
-#define container_of(ptr, type, member) \
-        ((type *)((void *)ptr - offsetof(type, member)))
-
 typedef int  ssize_t;
 typedef int  _mode_t;
 typedef int  _dev_t;
