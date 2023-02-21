a717cdae1268d7fbc7e90e92aafb2fc7c73adc03
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-21 09:31:14 +0800

    Use spinlock to protect memory pool

--- mm/mpool.c
+++ mm/mpool.c
@@ -7,16 +7,25 @@ void memory_pool_init(struct memory_pool *mem_pool, uint8_t *mem, size_t size)
 	mem_pool->offset = 0;
 	mem_pool->size = size;
 	mem_pool->mem = mem;
+	mem_pool->lock = 0;
 }
 
 void *memory_pool_alloc(struct memory_pool *mem_pool, size_t size)
 {
+	/* start of the critical section */
+	spin_lock(&mem_pool->lock);
+
+	void *addr = NULL;
+
 	/* test if size fits */
 	if((mem_pool->offset + size) <= mem_pool->size) {
 		uint8_t *alloc = mem_pool->mem + mem_pool->offset;
 		mem_pool->offset += size;
-		return alloc;
+		addr = alloc;
 	}
 
-	return NULL;
+	/* end of the critical section */
+	spin_unlock(&mem_pool->lock);
+
+	return addr;
 }
