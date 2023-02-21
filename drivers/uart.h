78f0643caba8c57d3aad4c4c15af3bd0180dee4c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-21 14:04:31 +0800

    Enable serial0 for shell input

--- drivers/uart.h
+++ drivers/uart.h
@@ -7,11 +7,8 @@ void serial0_init(void);
 
 void uart3_init(uint32_t baudrate);
 
-char uart3_getc(void);
-void uart3_puts(char *str);
-
 void uart_putc(USART_TypeDef *uart, char c);
 char uart_getc(USART_TypeDef *uart);
-void uart_puts(USART_TypeDef *uart, char *str);
+void uart_puts(USART_TypeDef *uart, const char *str, size_t size);
 
 #endif
