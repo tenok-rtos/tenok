e5e34cdcb2a85c8e2032e876022f00910a28b4c4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-23 10:41:18 +0800

    Replace write() with uart_puts() in mutex example

--- examples/mutex-ex.c
+++ examples/mutex-ex.c
@@ -11,13 +11,11 @@ void mutex_task1(void)
 {
 	set_program_name("mutex1");
 
-	int serial_fd = open("/dev/serial0", 0, 0);
-
 	char *str = "mutex task 1\n\r";
 
 	while(1) {
 		pthread_mutex_lock(&mutex_print);
-		write(serial_fd, str, strlen(str));
+		uart_puts(USART3, str, strlen(str));
 		pthread_mutex_unlock(&mutex_print);
 		sleep(100);
 	}
@@ -27,13 +25,11 @@ void mutex_task2(void)
 {
 	set_program_name("mutex2");
 
-	int serial_fd = open("/dev/serial0", 0, 0);
-
 	char *str = "mutex task 2\n\r";
 
 	while(1) {
 		pthread_mutex_lock(&mutex_print);
-		write(serial_fd, str, strlen(str));
+		uart_puts(USART3, str, strlen(str));
 		pthread_mutex_unlock(&mutex_print);
 		sleep(100);
 	}
