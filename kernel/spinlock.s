9970ff14cc9030f772297b6ccb64b331c417fa91
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-26 21:03:45 +0800

    spinlock.s: adjust the comments

--- kernel/spinlock.s
+++ kernel/spinlock.s
@@ -1,25 +1,29 @@
 .syntax unified
 
+/* lock is implemented by the test-and-set mechanism */
 .type   spin_lock, %function
 .global spin_lock
 spin_lock:
+//arguments:
+//r0 (input): address of the lock variable
 loop:   ldrex r2, [r0]     //r2 = *lock
         cmp   r2, #1       //if(r2 == 1)
-        beq   loop         //goto loop
+        beq   loop         //    goto loop
 
         mov   r1, #1       //r0 = 1
-        strex r2, r1, [r0] //[r0] = r1, r2 = result (success:0, failed:1)
+        strex r2, r1, [r0] //[r0] = r1, r2 = strex result (success:0, failed:1)
         cmp   r2, #1       //if(r2 == 1)
-        beq   loop         //goto loop
+        beq   loop         //    goto loop
 
-        bx    lr           //return
+        bx    lr           //function return
 
-/* unlock is easier since lock operation is exclusive and by writing
- * zero, the lock will be release */
+/* unluck is easier which requires only reseting the lock variable to zero */
 .type   spin_unlock, %function
 .global spin_unlock
+//arguments:
+//r0 (input): address of the lock variable
 spin_unlock:
-	mov   r1, #0       //r1 = 0
-	str   r1, [r0]     //[r0] = r1
+        mov   r1, #0       //r1 = 0
+        str   r1, [r0]     //[r0] = r1
 
-	bx    lr           //return
+        bx    lr           //function return
