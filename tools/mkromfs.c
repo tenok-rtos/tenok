1d4ca7da5b368d50b71509c52d5227543da7b55d
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-02-26 23:51:18 +0800

    fs: fs_create_file(): fix the empty entry name that treated as the file name

--- tools/mkromfs.c
+++ tools/mkromfs.c
@@ -256,6 +256,7 @@ static struct inode *fs_create_file(char *pathname, uint8_t file_type)
     struct inode *inode_curr = &inodes[0]; //start from the root node
     struct inode *inode;
 
+    char file_name[FILE_NAME_LEN_MAX];
     char entry[PATH_LEN_MAX];
     char *path = pathname;
 
@@ -268,6 +269,10 @@ static struct inode *fs_create_file(char *pathname, uint8_t file_type)
         /* two successive '/' are detected */
         if(entry[0] == '\0')
             continue;
+
+        /* the last non-empty entry string is the file name */
+        if(entry[0] != '\0')
+            strncpy(file_name, entry, FILE_NAME_LEN_MAX);
 
         /* search the entry and get the inode */
         inode = fs_search_file(inode_curr, entry);
@@ -289,17 +294,12 @@ static struct inode *fs_create_file(char *pathname, uint8_t file_type)
         } else {
             /* no more path to be splitted, the remained string should be the file name */
 
-            /* make sure the last char is not equal to '/' */
-            int len = strlen(pathname);
-            if(pathname[len - 1] == '/')
-                return NULL;
-
             /* file with the same name already exists */
             if(inode != NULL)
-                return -1;
+                return NULL;
 
             /* create new inode for the file */
-            inode = fs_add_file(inode_curr, entry, file_type);
+            inode = fs_add_file(inode_curr, file_name, file_type);
 
             /* failed to create the file */
             if(inode == NULL)
