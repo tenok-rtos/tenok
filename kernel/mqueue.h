99aa2b8d85a5342435384ac1173aca30e9f0c7f1
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-03-20 03:10:30 +0800

    Remove unimplement mq_close()

--- kernel/mqueue.h
+++ kernel/mqueue.h
@@ -25,6 +25,5 @@ struct mq_attr {
 mqd_t mq_open(const char *name, int oflag, struct mq_attr *attr);
 ssize_t mq_receive(mqd_t mqdes, char *msg_ptr, size_t msg_len, unsigned int *msg_prio);
 int mq_send(mqd_t mqdes, const char *msg_ptr, size_t msg_len, unsigned int msg_prio);
-int mq_close(mqd_t mqdes);
 
 #endif
