8208b2bd8eb21ce47b7f6e9d5246394ab68aa430
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-08-07 23:26:19 +0800

    Rename files of serial driver

--- drivers/drivers.mk
+++ drivers/drivers.mk
@@ -3,5 +3,5 @@ PROJ_ROOT := $(dir $(lastword $(MAKEFILE_LIST)))/../
 CFLAGS+=-I$(PROJ_ROOT)/drivers/serial
 
 SRC+=$(PROJ_ROOT)/drivers/serial/uart.c \
-	$(PROJ_ROOT)/drivers/serial/serial0.c \
-	$(PROJ_ROOT)/drivers/serial/serial1.c
+	$(PROJ_ROOT)/drivers/serial/console.c \
+	$(PROJ_ROOT)/drivers/serial/mavlink.c
