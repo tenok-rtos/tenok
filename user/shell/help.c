3c55b3a9a532bacde0aca27ec422fa7d8084aa07
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-02 18:49:52 +0800

    Fix omission error by help command

--- user/shell/help.c
+++ user/shell/help.c
@@ -18,18 +18,21 @@ void shell_cmd_help(int argc, char *argv[])
     for(int i = 0; i < shell_cmd_cnt; i++) {
         int cmd_len = strlen(shell_cmds[i].name);
 
-        if((buf_len + cmd_len + 1) < SHELL_PROMPT_LEN_MAX) {
-            strcat(s, shell_cmds[i].name);
-            strcat(s, " ");
-            buf_len += cmd_len + 1;
-        } else {
+        /* buffer is full, print the message out */
+        if((buf_len + cmd_len + 1) >= SHELL_PROMPT_LEN_MAX) {
             strcat(s, "\n\r");
             shell_puts(s);
             buf_len = 0;
             s[0] = '\0';
         }
+
+        /* append command name to the buffer */
+        strcat(s, shell_cmds[i].name);
+        strcat(s, " ");
+        buf_len += cmd_len + 1;
     }
 
+    /* print out the left message */
     if(buf_len > 0) {
         strcat(s, "\n\r");
         shell_puts(s);
