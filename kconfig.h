b063307a9215b1f33f987b5deaf967ce3949e6d5
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-04 23:51:47 +0800

    Increase size of kernel memory pool

--- kconfig.h
+++ kconfig.h
@@ -9,7 +9,7 @@
 #define TASK_MAX_PRIORITY    5
 #define TASK_NAME_LEN_MAX    50
 
-#define MEM_POOL_SIZE        4096
+#define MEM_POOL_SIZE        10240
 
 #define KERNEL_INT_PRI       5    /* isr priority higher than this value *
                                    * will not be disabled                */
