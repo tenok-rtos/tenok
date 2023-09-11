f4c32a43f49d0f5df34b7fa90804a8b6e7a130a2
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-11 18:18:56 +0800

    Remove ISR version of generic pipe read and write

--- include/kernel/pipe.h
+++ include/kernel/pipe.h
@@ -8,10 +8,6 @@
 typedef struct ringbuf pipe_t;
 
 pipe_t *generic_pipe_create(size_t nmem, size_t size);
-
-ssize_t generic_pipe_read_isr(pipe_t *pipe, char *buf, size_t size);
-ssize_t generic_pipe_write_isr(pipe_t *pipe, const char *buf, size_t size);
-
 ssize_t generic_pipe_read(pipe_t *pipe, char *buf, size_t size);
 ssize_t generic_pipe_write(pipe_t *pipe, const char *buf, size_t size);
 
