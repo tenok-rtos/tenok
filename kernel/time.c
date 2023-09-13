069c9ac73ac5f7f922deeb9dfe676fa2725186ef
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-13 20:14:49 +0800

    Fix clock time calculation

--- kernel/time.c
+++ kernel/time.c
@@ -9,8 +9,8 @@
 
 #define SYS_TIM_TICK_PERIOD (1.0f / OS_TICK_FREQ)
 
-uint32_t tick = 0;
-uint32_t time_s = 0;
+time_t time_s = 0;
+int tick = 0;
 
 void sys_time_update_handler(void)
 {
@@ -25,5 +25,5 @@ void sys_time_update_handler(void)
 int clock_gettime(clockid_t clockid, struct timespec *tp)
 {
     tp->tv_sec = time_s;
-    tp->tv_nsec = SYS_TIM_TICK_PERIOD * (float)tick * 1e9;
+    tp->tv_nsec = 1000000000 * tick / OS_TICK_FREQ;
 }
