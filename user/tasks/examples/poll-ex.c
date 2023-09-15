766cea83a9a8d8150d72b59a7f028ddbd9e9dee8
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-09-16 00:54:14 +0800

    Adjust example task of poll() syscall

--- user/tasks/examples/poll-ex.c
+++ user/tasks/examples/poll-ex.c
@@ -6,15 +6,31 @@
 #include <tenok/poll.h>
 #include <tenok/fcntl.h>
 #include <tenok/unistd.h>
+#include <tenok/sys/stat.h>
 
 char buffer[100];
 
-void poll_task(void)
+void poll_task1(void)
 {
-    set_program_name("poll");
+    set_program_name("poll1");
+
+    mkfifo("/poll_test", 0);
+    int fifo_fd = open("/poll_test", 0, 0);
+
+    char *s = "poll example.\n\r";
+
+    while(1) {
+        write(fifo_fd, s, strlen(s));
+        sleep(1);
+    }
+}
+
+void poll_task2(void)
+{
+    set_program_name("poll2");
 
     int serial_fd = open("/dev/serial0", 0, 0);
-    int mavlink_fd = open("/dev/serial1", 0, O_NONBLOCK);
+    int fifo_fd = open("/poll_test", 0, O_NONBLOCK);
 
     struct pollfd fds[1];
     fds[0].fd = serial_fd;
@@ -26,7 +42,7 @@ void poll_task(void)
         poll(fds, 1, -1);
 
         if (fds[0].revents & POLLIN) {
-            ssize_t rbytes = read(mavlink_fd, buffer, sizeof(buffer) - 1);
+            ssize_t rbytes = read(fifo_fd, buffer, sizeof(buffer) - 1);
             buffer[rbytes] = '\0';
 
             sprintf(s, "received %d bytes\n\r", rbytes);
@@ -37,4 +53,5 @@ void poll_task(void)
     }
 }
 
-HOOK_USER_TASK(poll_task);
+HOOK_USER_TASK(poll_task1);
+HOOK_USER_TASK(poll_task2);
