7e585ff0e678fba7796f2169f4be364539fa10c4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-10-05 20:04:33 +0800

    Revert "Remove thread terminated state"

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -317,6 +317,7 @@ static void thread_kill(struct thread_info *thread)
     list_remove(&thread->task_list);
     list_remove(&thread->thread_list);
     list_remove(&thread->list);
+    thread->status = THREAD_TERMINATED;
     bitmap_clear_bit(bitmap_threads, thread->tid);
     thread_cnt--;
 
@@ -405,6 +406,7 @@ static inline void thread_join_handler(void)
     list_remove(&running_thread->thread_list);
     list_remove(&running_thread->task_list);
     list_remove(&running_thread->list);
+    running_thread->status = THREAD_TERMINATED;
     bitmap_clear_bit(bitmap_threads, running_thread->tid);
     thread_cnt--;
 
@@ -505,6 +507,7 @@ void sys__exit(void)
         list_remove(&thread->thread_list);
         list_remove(&thread->task_list);
         list_remove(&thread->list);
+        thread->status = THREAD_TERMINATED;
         bitmap_clear_bit(bitmap_threads, thread->tid);
         thread_cnt--;
 
