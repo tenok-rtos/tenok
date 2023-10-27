076ce5016aff4eeb1024b63027098f8a123bdfec
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-10-27 20:25:02 +0800

    Rewrite ltoa()

--- kernel/printk.c
+++ kernel/printk.c
@@ -8,37 +8,45 @@
 #include <kernel/kfifo.h>
 #include <kernel/printk.h>
 
-#define PRINTK_BUF_LEN   100
+#define PRINTK_BUF_LTOA_BUF_LEN   100
 #define PRINTK_FIFO_SIZE 10
 
-void ltoa(char *buf, unsigned long i, int base)
+#define LTOA_BUF_LEN (sizeof(long) * 8)
+
+char *ltoa(long value, char *buffer, int radix)
 {
-#define LEN 25
+    long remainder;
+    char reverse_buffer[LTOA_BUF_LEN + 1] = {0};
+    char *last_digit = &reverse_buffer[LTOA_BUF_LEN - 1];
 
-    char *s;
-    int rem;
-    char rev[LEN + 1];
-
-    if (i == 0)
-        s = "0";
-    else {
-        rev[LEN] = 0;
-        s = &rev[LEN];
-
-        while (i) {
-            rem = i % base;
-
-            if (rem < 10) {
-                *--s = rem + '0';
-            } else if (base == 16) {
-                *--s = "abcdef"[rem - 10];
-            }
-
-            i /= base;
-        }
+    if (value < 0) {
+        *last_digit = '-';
+        last_digit--;
+        value *= -1;
+    } else if (value == 0) {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+        return buffer;
     }
 
-    strcpy(buf, s);
+    for(int i = 0; i < LTOA_BUF_LEN && value; i++) {
+        remainder = value % radix;
+
+        if (remainder < 10) {
+            *last_digit = remainder + '0';
+        } else if (radix == 16) {
+            *last_digit = "abcdef"[remainder - 10];
+        }
+
+        last_digit--;
+        value /= radix;
+    }
+
+    char *str = last_digit + 1;
+    size_t size = &reverse_buffer[LTOA_BUF_LEN] - last_digit;
+    strncpy(buffer, str, size);
+
+    return buffer;
 }
 
 void printk(char *format,  ...)
@@ -50,10 +58,10 @@ void printk(char *format,  ...)
     get_sys_time(&tp);
 
     char sec[15] = {0};
-    ltoa(sec, tp.tv_sec, 10);
+    ltoa(tp.tv_sec, sec, 10);
 
     char rem[15] = {0};
-    ltoa(rem, tp.tv_nsec, 10);
+    ltoa(tp.tv_nsec, rem, 10);
 
     char zeros[15] = {0};
     for(int i = 0; i < (9 - strlen(rem)); i++) {
