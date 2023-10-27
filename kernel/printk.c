2327cc0467e63a44b929676880469072ee852ee1
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-10-27 22:27:33 +0800

    Implement vsprintf() and sprintf()

--- kernel/printk.c
+++ kernel/printk.c
@@ -8,7 +8,7 @@
 #include <kernel/kfifo.h>
 #include <kernel/printk.h>
 
-#define PRINTK_BUF_LTOA_BUF_LEN   100
+#define PRINTK_BUF_LEN   100
 #define PRINTK_FIFO_SIZE 10
 
 #define ITOA_BUF_LEN (sizeof(int) * 8)
@@ -94,10 +94,10 @@ void printk(char *format,  ...)
     struct timespec tp;
     get_sys_time(&tp);
 
-    char sec[15] = {0};
+    char sec[sizeof(long) * 8 + 1] = {0};
     ltoa(tp.tv_sec, sec, 10);
 
-    char rem[15] = {0};
+    char rem[sizeof(long) * 8 + 1] = {0};
     ltoa(tp.tv_nsec, rem, 10);
 
     char zeros[15] = {0};
@@ -106,11 +106,68 @@ void printk(char *format,  ...)
     }
 
     char buf[100] = {0};
-
-    int pos = sprintf(buf, "[%5s.%s%s] ", sec, zeros, rem);
+    //int pos = sprintf(buf, "[%5s.%s%s] ", sec, zeros, rem);
+    int pos = sprintf(buf, "[%s.%s%s] ", sec, zeros, rem);
     pos += vsprintf(&buf[pos], format, args);
     pos += sprintf(&buf[pos], "\n\r");
     va_end(args);
 
     console_write(buf, strlen(buf));
+}
+
+int vsprintf(char *str, const char *format, va_list ap)
+{
+    str[0] = 0;
+    char append_str[2] = {0};
+
+    int pos = 0;
+    while(format[pos]) {
+        if(format[pos] != '%') {
+            append_str[0] = format[pos];
+            pos++;
+            strcat(str, append_str);
+            continue;
+        }
+
+        switch(format[++pos]) {
+            case 's': {
+                char *s = va_arg(ap, char *);
+                strcat(str, s);
+                break;
+            }
+            case 'c': {
+                append_str[0] = (char)va_arg(ap, int);
+                strcat(str, append_str);
+                break;
+            }
+            case 'd': {
+                int d = va_arg(ap, int);
+                char cast[sizeof(int) * 8 + 1];
+                strcat(str, itoa(d, cast, 10));
+                break;
+            }
+            case 'u': {
+                unsigned int u = va_arg(ap, unsigned int);
+                char cast[sizeof(unsigned int) * 8 + 1];
+                strcat(str, itoa(u, cast, 10));
+                break;
+            }
+            default:
+                break;
+        }
+
+        pos++;
+    }
+
+    return strlen(str);
+}
+
+int sprintf(char *str, const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int retval = vsprintf(str, format, args);
+    va_end(args);
+
+    return retval;
 }
