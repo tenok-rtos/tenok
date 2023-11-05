ba87808af52fb6b8ad4f2d82872e64388d858c9c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-11-05 22:15:51 +0800

    Define more errnos and add Doxygen comments

--- include/kernel/errno.h
+++ include/kernel/errno.h
@@ -4,6 +4,6 @@
 #ifndef __KERNEL_ERRNO_H__
 #define __KERNEL_ERRNO_H__
 
-#define ERESTARTSYS 512
+#define ERESTARTSYS 512 /**< Syscall requires restart */
 
 #endif
--- include/tenok/errno.h
+++ include/tenok/errno.h
@@ -4,20 +4,33 @@
 #ifndef __ERRNO_H__
 #define __ERRNO_H__
 
-#define EPERM     1
-#define ENOENT    2
-#define ESRCH     3
-#define EIO       5
-#define ENXIO     6
-#define EBADF     9
-#define EAGAIN    11
-#define ENOMEM    12
-#define EFAULT    14
-#define EBUSY     16
-#define EEXIST    17
-#define EINVAL    22
-#define EDEADLK   45
-#define EMSGSIZE  122
-#define EOVERFLOW 139
+#define EPERM     1   /**< Not the owner */
+#define ENOENT    2   /**< No such file or directory */
+#define ESRCH     3   /**< No such task */
+#define EINTR     4   /**< Syscall is interrupted */
+#define EIO       5   /**< I/O error */
+#define ENXIO     6   /**< No such device or address */
+#define E2BIG     7   /**< Argument list too long */
+#define EBADF     9   /**< Bad file descriptor number */
+#define EAGAIN    11  /**< Try again */
+#define ENOMEM    12  /**< Not enough memory */
+#define EACCES    13  /**< Permission denied */
+#define EFAULT    14  /**< Bad address */
+#define ENOTBLK   15  /**< Not a block device */
+#define EBUSY     16  /**< Device or resource busy */
+#define EEXIST    17  /**< File exists */
+#define EINVAL    22  /**< Invalid argument */
+#define ENFILE    23  /**< Too many open files in the system */
+#define EMFILE    24  /**< File descriptor value too long */
+#define ENOTTY    25  /**< Not a character device */
+#define ETXTBSY   26  /**< Text file busy */
+#define EFBIG     27  /**< File is too big */
+#define ENOSPC    28  /**< No space left */
+#define ESPIPE    29  /**< Illegal seek */
+#define EROFS     30  /**< Read-only file system */
+#define EDEADLK   45  /**< Deadlock */
+#define ENOSYS    88  /**< Function not implemented */
+#define EMSGSIZE  122 /**< Message to long */
+#define EOVERFLOW 139 /**< Numerical overflow */
 
 #endif
