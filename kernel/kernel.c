807370e918ee15f16d204ff79e6ab14b07f3b428
Shengwen Cheng <shengwen1997.tw@gmail.com>
2023-11-18 00:20:22 +0800

    Expand stack size for filesysd and softirqd

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3015,8 +3015,8 @@ void sched_start(void)
 
     /* initialize kernel threads and deamons */
     kthread_create(init, 0, 2048);
-    kthread_create(softirqd, KTHREAD_PRI_MAX, 1024);
-    kthread_create(filesysd, KTHREAD_PRI_MAX - 1, 1024);
+    kthread_create(softirqd, KTHREAD_PRI_MAX, 2048);
+    kthread_create(filesysd, KTHREAD_PRI_MAX - 1, 2048);
 
     /* manually set task 0 as the first thread to run */
     running_thread = &threads[0];
