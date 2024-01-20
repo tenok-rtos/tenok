cb8c082dd0eaeec09100664c0803d32e282c8a95
Shengwen Cheng <shengwen1997.tw@gmail.com>
2024-01-20 19:38:50 +0800

    Implement wait_event()

--- include/kernel/wait.h
+++ include/kernel/wait.h
@@ -7,7 +7,25 @@
 #include <stdbool.h>
 
 #include <common/list.h>
+#include <kernel/interrupt.h>  //XXX
+#include <kernel/kernel.h>
 #include <kernel/thread.h>
+
+#define DECLARE_WAIT_QUEUE_HEAD(wq) LIST_HEAD(wq)
+
+// XXX
+#define wait_event(wq_head, condition)                           \
+    do {                                                         \
+        CURRENT_THREAD_INFO(curr_thread);                        \
+        while (1) {                                              \
+            if (condition)                                       \
+                break;                                           \
+            preempt_disable();                                   \
+            prepare_to_wait(&wq_head, curr_thread, THREAD_WAIT); \
+            preempt_enable();                                    \
+            schedule();                                          \
+        }                                                        \
+    } while (0)
 
 /**
  * @brief  Suspend current thread and place it into a wait list with a new
