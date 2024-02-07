2e8b50ac63cfb44629e35de8debed9a8bcad5fc1
Shengwen Cheng <shengwen1997.tw@gmail.com>
2024-02-08 00:43:51 +0800

    Relocate serial/ into periph/

--- drivers/drivers.mk
+++ drivers/drivers.mk
@@ -1,11 +1,11 @@
 PROJ_ROOT := $(dir $(lastword $(MAKEFILE_LIST)))/../
 
 CFLAGS += -I $(PROJ_ROOT)/drivers/device
-CFLAGS += -I $(PROJ_ROOT)/drivers/serial
 CFLAGS += -I $(PROJ_ROOT)/drivers/periph
+CFLAGS += -I $(PROJ_ROOT)/drivers/periph/serial
 
-SRC += $(PROJ_ROOT)/drivers/serial/uart.c \
-       $(PROJ_ROOT)/drivers/serial/console.c \
-       $(PROJ_ROOT)/drivers/serial/mavlink.c \
-       $(PROJ_ROOT)/drivers/serial/debug_link.c \
+SRC += $(PROJ_ROOT)/drivers/periph/serial/uart.c \
+       $(PROJ_ROOT)/drivers/periph/serial/console.c \
+       $(PROJ_ROOT)/drivers/periph/serial/mavlink.c \
+       $(PROJ_ROOT)/drivers/periph/serial/debug_link.c \
        $(PROJ_ROOT)/drivers/periph/pwm.c
