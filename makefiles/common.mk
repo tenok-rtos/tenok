3886a094ea116852ada0ed1955d4bb50f555e06c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2026-08-25 19:58:40 +0800

    Let the linker drop what nothing reaches

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -16,8 +16,13 @@ MSG_DIR   := ./msg
 MSG_BUILD := ./build/msg
 
 LDFLAGS += -Wl,--no-warn-rwx-segments
+
+# Every function and every object gets a section of its own, so that the
+# linker can drop the ones nothing reaches
+LDFLAGS += -Wl,--gc-sections
 LDFLAGS += -lm
 
+CFLAGS += -ffunction-sections -fdata-sections
 CFLAGS += -O2 -g -mlittle-endian -mthumb \
           -fcommon \
           -mcpu=cortex-m4 \
